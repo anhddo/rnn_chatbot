603a9cd3db95f78ad6d8475bc9dbb6e1bdb93e81
anhddo <anhddo93@gmail.com>
2018-01-15 18:03:00 +0700

    bigger milestone

--- train.py
+++ train.py
@@ -40,7 +40,10 @@ def train():
     iter_idx = 0 if len(ckpts) == 0 else max(ckpts)
     print_loss_total = 0.0
     milestones = [total_batch] * 15
-    milestones[0] = total_batch * 5
+    milestones[0] = total_batch * 10
+    milestones[1] = total_batch * 5
+    milestones[2] = total_batch * 5
+    milestones[3] = total_batch * 2
     milestones = np.cumsum(milestones)
 
     n_iters = milestones[-1]
